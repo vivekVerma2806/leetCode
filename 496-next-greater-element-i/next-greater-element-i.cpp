b7801d0738dac4c5ce5242d43fc6d02e2fae4921
Vivek Kumar Verma <vivekkumarverma664@gmail.com>
2026-07-13 14:55:08 +0530

    Time: 0 ms (100.00%) | Memory: 13.1 MB (15.63%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.cpp
+++ 496-next-greater-element-i/next-greater-element-i.cpp
@@ -1,22 +1,28 @@
 class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-        int n = nums1.size();
-     vector<int>ans(n,-1);
-        for(int i =0  ;i < nums1.size() ;i++){
-            for(int j =0; j < nums2.size();j++){
-                if(nums1[i]==nums2[j]){
-                    
-                    for(int k = j+1 ; k< nums2.size() ;k++ ){
-                        if(nums2[k]>nums2[j]){
-                            ans[i]=nums2[k];
-                            break;
-                        }
-                    }
-                }
+        unordered_map<int,int>mp;//nums2 ke value aur ush value ke rgiht side main next greater ele 
+        stack<int>st;
+        for(int i = nums2.size()-1 ; i>=0; i--){
+
+              while (!st.empty() && st.top() <= nums2[i]) {
+                st.pop();
             }
+
+             // If stack is empty, no greater element exists
+            if (st.empty()) {
+                mp[nums2[i]] = -1;
+            } else {
+                mp[nums2[i]] = st.top();
+            }
+
+            st.push(nums2[i]);
+        }
+      vector<int>ans;
+        for(int num : nums1){
+             ans.push_back(mp[num]);
         }
 
-return ans;
+        return ans;
     }
 };
