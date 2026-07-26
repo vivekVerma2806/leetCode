f48851be1eae7299c589a256ad48da620022e1f8
Vivek Kumar Verma <vivekkumarverma664@gmail.com>
2026-07-26 13:42:45 +0530

    Time: 0 ms (100.00%) | Memory: 31.6 MB (10.45%) - LeetSync

--- 628-maximum-product-of-three-numbers/maximum-product-of-three-numbers.cpp
+++ 628-maximum-product-of-three-numbers/maximum-product-of-three-numbers.cpp
@@ -4,7 +4,7 @@ public:
         int n =nums.size();
         sort(nums.begin(),nums.end());
         
-         if(nums[0]<0 && nums[1]<0 && nums[n-1]>=0){
+        
             // yaha 2 case bane gaya 
             // case 1 
             int case1=nums[0]*nums[1]*nums[n-1];
@@ -12,8 +12,6 @@ public:
             // case2 
             int case2= nums[n-1]*nums[n-2]*nums[n-3];
             return max(case1,case2);
-        }else{
-            return nums[n-1]*nums[n-2]*nums[n-3];
-        }
+     
     }
 };
