cf5861eb8afb49c8dc5f02431c4c67116070b548
Vivek Kumar Verma <vivekkumarverma664@gmail.com>
2026-07-31 17:30:14 +0530

    Time: 38 ms (18.40%) | Memory: 27 MB (43.22%) - LeetSync

--- 3276-minimum-number-of-pushes-to-type-word-ii/minimum-number-of-pushes-to-type-word-ii.cpp
+++ 3276-minimum-number-of-pushes-to-type-word-ii/minimum-number-of-pushes-to-type-word-ii.cpp
@@ -1,42 +1,23 @@
 class Solution {
 public:
     int minimumPushes(string word) {
-        // mujhe diff word diya gaye hain aur mujhe jo word jade bar aaye hain usko frist dena 
-        // hoga then uske accoding 
-        // step 1 freq ke accoding soet hogaya 
-         unordered_map<char, int> freq;
-            for (char ch : word) {
-                      freq[ch]++;
-                  }
-           vector<pair<char,int>>v(freq.begin(),freq.end());
-           sort(v.begin(),v.end(),[](auto &a ,auto &b){
-                 return a.second > b.second;
-           });
-        // ab 1--8 tak ke char ko jo jade freq wale hain usko 1 dete hain 
-        // 9--16 tak wale char ko 2 dena hogaya 
-        // 17-24 wale ko 3 dena hogaya 
-        // 25--26 wale ko 4 dena hogaya 
-        
-        // char aur uska rank dal dete hain 
-        unordered_map<char,int>rank;
-        int count=1;
-        for(auto it : v){
-            // it.first== char 
-            if(count<=8){
-                rank[it.first]=1;
-            }else if( 8 < count &&count<=16){
-                 rank[it.first]=2;
-            }else if(16<count && count<=24){
-                rank[it.first]=3;
-            }else{
-                rank[it.first]=4;
-            }
-            count++;
-        }
-        int ans=0;
-         for(char ch : word){
-            ans+=rank[ch];
-         }
-        return ans;
+       unordered_map<char,int> freq;
+
+for(char ch : word)
+    freq[ch]++;
+
+vector<pair<char,int>> v(freq.begin(), freq.end());
+
+sort(v.begin(), v.end(), [](auto &a, auto &b){
+    return a.second > b.second;
+});
+
+int ans = 0;
+
+for(int i = 0; i < v.size(); i++){
+    ans += (i / 8 + 1) * v[i].second;
+}
+
+return ans;
     }
 };
