22dae7246ff64974222d63d04742363d519b1504
Vivek Kumar Verma <vivekkumarverma664@gmail.com>
2026-09-18 01:02:55 +0530

    Time: 1014 ms (71.72%) | Memory: 0B (100.00%) - LeetSync

--- 1773-percentage-of-users-attended-a-contest/percentage-of-users-attended-a-contest.sql
+++ 1773-percentage-of-users-attended-a-contest/percentage-of-users-attended-a-contest.sql
@@ -1,10 +1,7 @@
-# Write your MySQL query statement below
-SELECT r.contest_id, ROUND((COUNT(DISTINCT u.user_id)*100/(SELECT COUNT(*) FROM Users)),2) AS percentage
-FROM Users u 
-JOIN Register r
-ON u.user_id = r.user_id
+SELECT 
+    r.contest_id,
+   ROUND(( COUNT(DISTINCT r.user_id) * 100.0 /
+    (SELECT COUNT(*) FROM Users)),2)  AS percentage
+FROM Register r
 GROUP BY r.contest_id
-ORDER BY percentage DESC ,r.contest_id  ;
-
-#conets ko koun karo kitne hain  
-# mujhe count karna hain kitne bar reg_table main  kitne bar aaya hain  / aur ush main kitne diff user ne reg ke hain 
+ORDER BY percentage DESC ,r.contest_id;
